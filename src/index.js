65f79323dff59c0f50a0ad033a291a45f5f8846c
Travis P <76796661+traffis@users.noreply.github.com>
2024-10-11 17:57:44 -0400

    Move index.html and index.js out of src/ folder for GitHub Pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-let bio = document.getElementById("bio");
-let avatar = document.getElementById("avatar");
-
-fetch("https://api.github.com/users/traffis")
-  .then((response) => response.json())
-  .then((data) => {
-    bio.innerHTML = data.bio;
-    avatar.src = data.avatar_url;
-  });
